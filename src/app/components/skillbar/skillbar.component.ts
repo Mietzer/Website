3e376398b5b4e32f454ee0b6a472807420553b28
Mietzey <mietzey2000@gmail.com>
2025-03-28 23:43:14 +0100

    made adjustments to skillbar and its position, TODO: Clean Up of CSS and HTML Code

--- src/app/components/skillbar/skillbar.component.ts
+++ src/app/components/skillbar/skillbar.component.ts
@@ -9,16 +9,16 @@ import { Component } from '@angular/core';
 export class SkillbarComponent {
 
   skills = [
-    { name: "HTML", projects: "Website", logo: "CSSHTML.svg"},
-    { name: "CSS", projects: "Website", logo: "CSSHTML.svg"},
+    { name: "HTML", projects: "Website", logo: "HTML.svg"},
+    { name: "CSS", projects: "Website", logo: "CSS.svg"},
     { name: "JavaScript", projects: "Website", logo: "JAVASCRIPT.svg"},
     { name: "TypeScript", projects: "Website", logo: "TYPESCRIPT.svg"},
-    { name: "Angular", projects: "Website", logo: "ANGULAR.svg"},
-    { name: "Visual Studio Code", projects: "Website", logo: "VISUALSTUDIOCODE.svg"},
+    { name: "Angular", projects: "Website", logo: "ANGULAR.ico"},
     { name: "C#", projects: "MTGApp", logo: "CSHARP.svg"},
     { name: "Java", projects: "MTGApp", logo: "JAVA.svg"},
     { name: "Python", projects: "MTGApp", logo: "PYTHON.svg"},
     { name: "MySQL", projects: "MTGApp", logo: "MYSQL.svg"},
+    {name: "Visual Studio Code", projects: "Website", logo: "VISUALSTUDIOCODE.svg"},
     { name: "Visual Studio", projects: "MTGApp", logo: "VISUALSTUDIO.svg"},
     { name: "IntelliJ", projects: "MTGApp", logo: "INTELLIJ.svg"},
   ]
